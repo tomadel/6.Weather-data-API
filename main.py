e61d37781cdbba8afb1d1f6cdffaed5c90349490
Guney <aliguneydogan@hotmail.com>
2025-02-11 18:43:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask("Website")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+app.run(debug=True)
