3a72cec2792d2c38059ab5df78593125becd5bfd
Guney <aliguneydogan@hotmail.com>
2025-02-12 18:03:18 +0300

    Show specific years in the API

--- main.py
+++ main.py
@@ -20,5 +20,21 @@ def get_weather(station, date):
             "temperature": temperature}
 
 
+
+@app.route("/api/v1/<station>")
+def get_a_station_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def get_a_station_data_for_year(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict()
+    return result
+
 if __name__ == "__main__":
     app.run(debug=True)
